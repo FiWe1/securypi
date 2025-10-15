27758a1666f2a03715fe3439c559708befc47f91
FiWe1 <filipweissen@gmail.com>
2025-10-15 11:04:27 +0200

    Revert "take picture fix"
    This reverts commit d0d79ea7565b01443bfed585ea5369e77b21f817.

--- securyflask/mycam.py
+++ securyflask/mycam.py
@@ -36,11 +36,6 @@ class MyPicamera2(Picamera2):
     
     def configureAndTakePicture(self, fileOutput):
         self.configure(self.create_still_configuration(main={"size": (1920, 1080)}))
-        request = self.capture_request()
-        buffer = request.make_buffer("main")
-        
-        fileOutput.write(buffer)
-        
-        request.release()
+        self.start_recording(JpegEncoder(), FileOutput(fileOutput))
         
         return self
