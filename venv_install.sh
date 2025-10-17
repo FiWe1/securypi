58750d3c05ac43538fb9853f73dfa568cd4ebe67
FiWe1 <filipweissen@gmail.com>
2025-10-17 17:09:25 +0200

    installscript fix

--- venv_install.sh
+++ venv_install.sh
@@ -10,7 +10,7 @@ echo "Installing required packages from requirements.txt..."
 # Ensure numpy and simplejpeg have compatible builds
 .venv/bin/python -m pip install --upgrade pip setuptools wheel
 .venv/bin/python -m pip install --upgrade numpy
-.venv/bin/python -m pip install --upgrade --force-reinstall --no-binary=:all: simplejpeg
+.venv/bin/python -m pip install --upgrade --force-reinstall simplejpeg
 
 # Install the rest of the requirements
 .venv/bin/python -m pip install -r requirements.txt
