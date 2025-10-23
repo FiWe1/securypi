ccd6f9349c935106ad1f2b67b6072425738f8c2c
FiWe1 <filipweissen@gmail.com>
2025-10-23 02:22:23 +0200

    update count project lines

--- tests/count_project_lines.sh
+++ tests/count_project_lines.sh
@@ -4,9 +4,19 @@
 # of a python project,
 # run in project directory.
 
+# to exclude directory:
+# ! -path "*/dirname/*"
+
 find . -type f \
   ! -path "*/__pycache__/*" \
   ! -path "*/.venv/*" \
   ! -path "*/.git/*" \
-  ! -path "*/picamera_tests/*" \ # exclude directories
+  ! -path "*/picamera_tests/*" \
+  ! -path "*/instance/*" \
+  ! -path "*/tests/*" \
   -exec wc -l {} + | sort -n
+
+# to include a .file type:
+# add to OR logic: \( ... -o ... -o -name "*.file" \)
+
+# \( -name "*.py" -o -name "*.html" -o -name "*.css" \) \
