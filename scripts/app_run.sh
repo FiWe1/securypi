2d322cd5a87645b21670324bba07f03478f12fb4
FiWe1 <filipweissen@gmail.com>
2025-11-07 00:43:43 +0100

    minor fixez

--- scripts/app_run.sh
+++ scripts/app_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-.venv/bin/python -m flask --app flaskurypi run -h localhost -p 5554 --debugger
+.venv/bin/python -m flask --app flaskurypi run -h localhost -p 5555 --debugger
 
 # change "-h [address]" to 0.0.0.0 for all networks - unsafe!
 # use VPN ip
