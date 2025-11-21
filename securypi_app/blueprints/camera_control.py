c9b63edf5ea86fca3e59915301081b48fde37161
FiWe1 <filipweissen@gmail.com>
2025-11-21 15:49:31 +0100

    move recording to camera_control

--- securypi_app/blueprints/camera_control.py
+++ securypi_app/blueprints/camera_control.py
@@ -1,15 +1,53 @@
-from flask import Blueprint
-from flask import render_template
+from datetime import datetime
+
+from flask import Blueprint, render_template, redirect, url_for, flash
 
 from securypi_app.services.auth import login_required
+from securypi_app.sensors.mycam import MyPicamera2, Quality
 
 
 ### Globals ###
 bp = Blueprint("camera_control", __name__, url_prefix="/camera_control")
 
 
+@bp.route("/start_recording")
+@login_required
+def start_recording():
+    camera = MyPicamera2.get_instance()
+    filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".mp4"
+    path = "recording_demo/"
+    full_path = path + filename
+
+    try:
+        camera.start_recording_to_file(full_path,
+                                       stream="main",
+                                       encode_quality=Quality.LOW)
+    except Exception as e:
+        print(f"Error starting recording: {e}")
+        flash("Error starting recording.")
+
+    return redirect(url_for("recordings.index"))
+
+
+@bp.route("/stop_recording")
+@login_required
+def stop_recording():
+    camera = MyPicamera2.get_instance()
+
+    try:
+        camera.stop_recording_to_file()
+    except Exception as e:
+        print(f"Error stopping recording: {e}")
+        flash("Error stopping recording.")
+
+    return redirect(url_for("recordings.index"))
+
+
 @bp.route("/")
 @login_required
 def index():
+    camera = MyPicamera2.get_instance()
+    is_recording = camera.is_recording()
     """ Default (inde) route for camera_control blueprint."""
-    return render_template("camera_control.html")
+    return render_template("camera_control.html",
+                           is_recording=is_recording)
--- securypi_app/blueprints/recordings.py
+++ securypi_app/blueprints/recordings.py
@@ -1,53 +1,14 @@
-from datetime import datetime
-
-from flask import Blueprint, render_template, redirect, url_for, flash
+from flask import Blueprint, render_template
 
 from securypi_app.services.auth import login_required
-from securypi_app.sensors.mycam import MyPicamera2, Quality
 
 
 ### Globals ###
 bp = Blueprint("recordings", __name__, url_prefix="/recordings")
 
 
-@bp.route("/start_recording")
-@login_required
-def start_recording():
-    camera = MyPicamera2.get_instance()
-    filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".mp4"
-    path = "recording_demo/"
-    full_path = path + filename
-
-    try:
-        camera.start_recording_to_file(full_path,
-                                       stream="main",
-                                       encode_quality=Quality.LOW)
-    except Exception as e:
-        print(f"Error starting recording: {e}")
-        flash("Error starting recording.")
-
-    return redirect(url_for("recordings.index"))
-
-
-@bp.route("/stop_recording")
-@login_required
-def stop_recording():
-    camera = MyPicamera2.get_instance()
-
-    try:
-        camera.stop_recording_to_file()
-    except Exception as e:
-        print(f"Error stopping recording: {e}")
-        flash("Error stopping recording.")
-
-    return redirect(url_for("recordings.index"))
-
-
 @bp.route("/")
 @login_required
 def index():
     """ Default (index) route for recordings blueprint. """
-    camera = MyPicamera2.get_instance()
-    is_recording = camera.is_recording()
-    return render_template("recordings.html",
-                           is_recording=is_recording)
+    return render_template("recordings.html")
