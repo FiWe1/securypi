a34addf542ce9a87a5ffa3f1715b9c414f62e70c
FiWe1 <filipweissen@gmail.com>
2025-11-25 00:24:26 +0100

    test weather sensor

---
+++ tests/sensors/test_weather_sensor.py
@@ -0,0 +1,76 @@
+import pytest
+
+from securypi_app import create_app
+from securypi_app.sensors.weather_sensor import WeatherSensor
+
+
+class TestWeatherSensor():
+    
+    @pytest.fixture
+    def app(self):
+        """
+        Create the app.
+        To import the app context, add 'app' parameter to the thest.
+        """
+        app = create_app()
+        with app.app_context():
+            yield app
+
+    
+    @pytest.fixture
+    def sensor(self, app):
+        """
+        Every time yield the same WeatherSensor instance,
+        but with default configuration.
+        """
+        sensor = WeatherSensor.get_instance()
+        yield sensor
+
+        # try to get WeatherSensor to default state
+        sensor._background_logger_stop_event.set()
+        if sensor.is_background_logging():
+            sensor._background_logger_thread.join()
+            sensor._background_logger_thread = None
+        sensor._background_logger_stop_event.clear()
+
+        del sensor  # delete object reference
+    
+    def test_singleton(self, sensor):
+        """ Try to break singleton """
+        obj2 = WeatherSensor()
+        assert sensor is obj2
+
+    def test_get_temperature(self, sensor):
+        try:
+            temp = sensor.get_temperature()
+            hum = sensor.get_temperature()
+        except:
+            # might fail - not handling exceptions
+            return
+        
+        assert isinstance(temp, float)
+        assert isinstance(hum, float)
+    
+    def test_measure(self, sensor):
+        res = sensor.measure()
+        if res == None:
+            return
+        
+        assert isinstance(res["temperature"], float)
+        assert isinstance(res["humidity"], float)
+    
+    def test_measure_or_na(self, sensor):
+        pass
+    
+    def test_background_logger_start():
+        pass
+    
+    def test_background_logger():
+        pass
+        # @TODO - check results appearing in db
+    
+    def test_c_to_fahrenheit(celsius: float):
+        pass
+    
+    def test_f_to_celsius(self, fahrenheit: float):
+        pass
