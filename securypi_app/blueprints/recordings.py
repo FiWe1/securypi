644311efcb4d86835ffd72cb2881fa0767f48ec1
FiWe1 <filipweissen@gmail.com>
2025-12-05 14:04:12 +0100

    recordings batch delete, download zip stream

--- securypi_app/blueprints/recordings.py
+++ securypi_app/blueprints/recordings.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, send_from_directory, current_app, flash,
+    Blueprint, request, Response, render_template, send_from_directory, current_app, flash,
     redirect, url_for
 )
 
@@ -7,7 +7,9 @@ from securypi_app.services.auth import login_required
 from securypi_app.services.captures import (
     list_motion_captures, motion_captures_full_path, is_motion_capture_valid,
     list_recordings, recordings_full_path, is_recording_valid,
+    delete_motion_captures, delete_recordings, create_zip_stream
 )
+from securypi_app.services.auth import is_logged_in_admin
 
 
 ### Globals ###
@@ -33,11 +35,49 @@ def download_recording(filename):
     return redirect(url_for("recordings.index"))
 
 
+def handle_batch_form_action(form):
+    action = form.get('action')
+    
+    motion_captures = list_motion_captures()
+    recordings = list_recordings()
+    
+    all_motion_captures = []
+    all_recordings = []
+    for motion in motion_captures:
+        if form.get(motion):
+            all_motion_captures.append(motion)
+    for rec in recordings:
+        if form.get(rec):
+            all_recordings.append(rec)
+    
+    
+    # only admin can delete
+    if action == "delete_selected" and is_logged_in_admin():
+        delete_motion_captures(all_motion_captures)
+        delete_recordings(all_recordings)
+                
+    elif action == "download_selected":
+        zs = create_zip_stream(all_motion_captures,
+                                      all_recordings,
+                                      current_app.root_path)
+        return Response(
+            zs,
+            mimetype="application/zip",
+            headers={
+                "Content-Disposition": f"attachment; filename=selected_captures.zip"
+            }
+        )
+    
+    return redirect(url_for("recordings.index"))
+    
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 @login_required
 def index():
     """ Default (index) route for recordings blueprint. """
+    if request.method == "POST":
+        return handle_batch_form_action(request.form)
+    
     motion_captures = list_motion_captures()
     recordings = list_recordings()
     
--- securypi_app/services/captures.py
+++ securypi_app/services/captures.py
@@ -2,7 +2,9 @@
 Helper functions for accessing captured videos in: captures/...
 """
 import os
+from pathlib import Path
 from werkzeug.utils import secure_filename
+import zipstream
 
 
 # "captures/motion_captures"
@@ -11,12 +13,23 @@ def list_motion_captures() -> list[str]:
 
 
 def motion_captures_full_path(current_app_root_path: str) -> str:
-    project_root = os.path.abspath(os.path.join(current_app_root_path, ".."))
-    return os.path.join(project_root, "captures", "motion_captures")
+    project_root = Path(current_app_root_path).parent.resolve()
+    return str(project_root / "captures" / "motion_captures")
 
 
 def is_motion_capture_valid(filename: str) -> bool:
     return secure_filename(filename) in list_motion_captures()
+
+
+def delete_motion_capture(filename: str) -> None:
+    path = Path("captures") / "motion_captures" / filename
+    if path.exists():
+        path.unlink() # delete
+
+
+def delete_motion_captures(motion_captures: list[str]) -> None:
+    for motion in motion_captures:
+        delete_motion_capture(motion)
 
 
 # "captures/recordings"
@@ -25,9 +38,42 @@ def list_recordings() -> list[str]:
 
 
 def recordings_full_path(current_app_root_path: str) -> str:
-    project_root = os.path.abspath(os.path.join(current_app_root_path, ".."))
-    return os.path.join(project_root, "captures", "recordings")
+    project_root = Path(current_app_root_path).parent.resolve()
+    return str(project_root / "captures" / "recordings")
 
 
 def is_recording_valid(filename: str) -> bool:
     return secure_filename(filename) in list_recordings()
+
+
+def delete_recording(filename: str) -> None:
+    path = Path("captures") / "recordings" / filename
+    if path.exists():
+        path.unlink() # delete
+
+
+def delete_recordings(recordings: list[str]) -> None:
+    for rec in recordings:
+        delete_recording(rec)
+
+
+# zip
+def create_zip_stream(motion_captures: list[str],
+                      recordings: list[str],
+                      current_app_root_path: str) -> zipstream:
+    """ Return zip stream with motion captures and recordings """
+    zip = zipstream.ZipStream()
+
+    mc_full_path = Path(motion_captures_full_path(current_app_root_path))
+    downloads_fullpath_name = [
+        (str(mc_full_path / motion), motion) for motion in motion_captures
+    ]
+    r_full_path = Path(recordings_full_path(current_app_root_path))
+    downloads_fullpath_name.extend([
+        (str(r_full_path / rec), rec) for rec in recordings
+    ])
+    
+    for full_path, name in downloads_fullpath_name:
+        zip.add(full_path, name)
+    
+    return zip
