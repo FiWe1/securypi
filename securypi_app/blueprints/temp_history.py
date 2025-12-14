9bca74875263233e155e4dfe7732dd04dd98247f
FiWe1 <filipweissen@gmail.com>
2025-12-14 15:14:11 +0100

    Measurement fetch_previous_range, to_local_timezone

--- securypi_app/blueprints/temp_history.py
+++ securypi_app/blueprints/temp_history.py
@@ -13,6 +13,13 @@ bp = Blueprint("temp_history", __name__, url_prefix="/temp_history")
 @login_required
 def index():
     """ Default (index) route for temp_history blueprint. """
-    # test
-    Measurement.testprintall()
+    # init WeatherStation to start logging
+    weather_station = WeatherStation.get_instance()
+    
+    # cli measurements test
+    last24 = map(Measurement.to_local_timezone,
+                 Measurement.fetch_previous_range(days_before=1))
+    for m in last24:
+        print(m)
+    
     return render_template("temp_history.html")
--- securypi_app/models/measurement.py
+++ securypi_app/models/measurement.py
@@ -1,10 +1,15 @@
 from __future__ import annotations  # fix class forward referencing issue
 
-from datetime import datetime
+from zoneinfo import ZoneInfo
+from datetime import datetime, timedelta, timezone
 from sqlalchemy import Integer, Float, DateTime, select, text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from . import db
+
+
+# @TODO move to json config
+TIMEZONE_ZONEINFO = "Europe/Prague"
 
 
 class Measurement(db.Model):
@@ -14,7 +19,9 @@ class Measurement(db.Model):
         Integer, primary_key=True, autoincrement="auto"
     )
     time: Mapped[datetime] = mapped_column(
-        DateTime, nullable=False, server_default=text("CURRENT_TIMESTAMP")
+        DateTime(timezone=True), # hadnling only UTC time
+        nullable=False,
+        server_default=text("CURRENT_TIMESTAMP") # is UTC
     )
     temperature: Mapped[float] = mapped_column(
         Float, nullable=True
@@ -32,20 +39,18 @@ class Measurement(db.Model):
             f"temperature={self.temperature}, humidity={self.humidity}, "
             f"pressure={self.pressure})"
         )
-
-    @classmethod
-    def testprintall(cls) -> None:
-        measurements = db.session.execute(select(cls))
-        for m in measurements.scalars():
-            print(m)
-
-    @classmethod
-    def fetch_latest(cls) -> Measurement | None:
-        stmt = (select(cls)
-                .order_by(cls.time.desc())
-                .limit(1)
-                )
-        return db.session.execute(stmt).scalar_one_or_none()
+    
+    def to_local_timezone(self, local_timezone: ZoneInfo = None) -> Measurement:
+        """
+        Converts Measurement to local time zone. (implicitly from config)
+        """
+        if local_timezone is None:
+            # @TODO fetch from json config
+            local_timezone = ZoneInfo(TIMEZONE_ZONEINFO)
+        
+        tm = self.time.replace(tzinfo=timezone.utc)
+        self.time = tm.astimezone(local_timezone)
+        return self
 
     def log(self) -> bool:
         db.session.add(self)
@@ -57,3 +62,56 @@ class Measurement(db.Model):
             return True
 
         return False
+
+    @classmethod
+    def fetch_latest(cls) -> Measurement | None:
+        stmt = (select(cls)
+                .order_by(cls.time.desc())
+                .limit(1)
+                )
+        return db.session.execute(stmt).scalar_one_or_none()
+
+    @classmethod
+    def fetch_previous_range(cls,
+                             target_datetime_timezone: datetime = None,
+                             days_before: int = 0,
+                             hours_before: int = 0,
+                             minutes_before: int = 0) -> list[Measurement]:
+        """
+        explicit 'target_datetime_timezone' must have a timezone information!
+        (datetime.now(timezone.utc))
+        
+        Retrieves list of measurements in interval:
+        <(target_datetime - ..._before); target_datetime>
+        By default returns measurements from last 24 hours.
+        """
+        # input check:
+        if target_datetime_timezone is not None and (
+           target_datetime_timezone.utcoffset() is None):
+            raise ValueError("Naive datetime is not allowed; timezone required")
+        
+        # safe default:
+        if target_datetime_timezone is None:
+            target_datetime_timezone = datetime.now(timezone.utc)
+        if days_before == 0 and hours_before == 0 and minutes_before == 0:
+            hours_before = 24
+
+        delta = timedelta(
+            days=days_before,
+            hours=hours_before,
+            minutes=minutes_before
+        )
+        datetime_from = target_datetime_timezone - delta
+        
+        stmt = (select(cls)
+                .where(cls.time >= datetime_from)
+                .where(cls.time <= target_datetime_timezone)
+                .order_by(cls.time.asc())
+                )
+        return db.session.execute(stmt).scalars().all()
+    
+    @classmethod
+    def testprintall(cls) -> None:
+        measurements = db.session.execute(select(cls))
+        for m in measurements.scalars():
+            print(m)
